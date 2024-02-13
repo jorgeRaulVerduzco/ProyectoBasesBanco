b8d750c6393703f32f90f836b6226a207d82a802
jorgeRaulVerduzco <159949132+jorgeRaulVerduzco@users.noreply.github.com>
2024-02-13 12:38:54 -0700

    Add files via upload

---
+++ ProyectoBancoBD/src/main/java/practica01_252181/proyectobancobd/ProyectoBancoBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package practica01_252181.proyectobancobd;
+
+/**
+ *
+ * @author lv1821
+ */
+public class ProyectoBancoBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
